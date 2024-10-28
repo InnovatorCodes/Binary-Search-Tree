a55ca4556a7cea8875fbcbff23327b84f8d1c8a1
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-10-28 21:46:12 +0530

    Completed deleteItem method

--- script.js
+++ script.js
@@ -61,11 +61,44 @@ function Tree(arr){
     }
     
     const deleteItem=(value)=>{
-
+        let parentNode=null;
+        let delNode=root;
+        while(delNode!=null && delNode.data!=value){
+            parentNode=delNode;
+            if(value<delNode.data) delNode=delNode.left;
+            else if(value>delNode.data) delNode=delNode.right;
+        }
+        if(delNode==null) return false;
+        else{
+            if(!(delNode.left || delNode.right)){
+                if(delNode.data>parentNode.data) parentNode.right=null;
+                else parentNode.left=null;
+            }
+            else if(delNode.left && !delNode.right){
+                delNode.data=delNode.left.data;
+                delNode.left=null;
+            }
+            else if(delNode.right && !delNode.left){
+                delNode.data=delNode.right.data;
+                delNode.right=null;
+            }
+            else{
+                let smallestNode=delNode.right;
+                parentNode=delNode;
+                while(smallestNode.left){
+                    parentNode=smallestNode;
+                    smallestNode=smallestNode.left;
+                }
+                let smallestValue=smallestNode.data;
+                if(smallestNode.data>parentNode.data) parentNode.right=null;
+                else parentNode.left=null;
+                delNode.data=smallestValue;
+            }
+        }
     }
     
     buildTree(arr);
-    return {root,buildTree,prettyPrint,find,insert};
+    return {root,buildTree,prettyPrint,find,insert,deleteItem};
 }
 
 let tree=Tree([1,2,2,3,4,5,6,6,6,7]);
@@ -73,3 +106,6 @@ tree.insert(25);
 console.log(tree);
 tree.prettyPrint();
 console.log(tree.find(4));
+tree.deleteItem(25);
+tree.deleteItem(6);
+tree.prettyPrint();
